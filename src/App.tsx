794db3af8287e215f786ac8143cd7f3c4b28cef7
Apoorva Sharma <kannusharma524@gmail.com>
2026-02-27 23:03:54 +0530

    feat: commiting initial starter project files

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-4xl font-bold text-white">
+        App
+      </h1>
+    </div>
+  )
+}
+
